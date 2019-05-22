cc452317d1d8031bc428a8b17e296f7e08b4d969
kato-syogo <kato-syogo@dmm.com>
2019-05-22 12:14:32 +0900

    codesandbox反映用

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+import './component/test'; 
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <test-element></test-element>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Router } from 'react-router-dom';
+import { Switch } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createBrowserHistory();
+
+ReactDOM.render(
+    <Router history={history}>
+        <Switch>
+            <Route exact path="/" component={App} />
+        </Switch>
+    </Router>
+    ,document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
